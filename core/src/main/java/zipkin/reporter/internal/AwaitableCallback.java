74cb93b2624a5e2495c2b5705c7b10eb0353fd74
Adrian Cole <adriancole@users.noreply.github.com>
2017-07-06 20:30:33 +0800

    Fix bug where we set interrupt on AwaitableCallback.await (#50)
    `AwaitableCallback.await()`, a test utility was setting interrupt when
    `Callback.onError(InterruptedException)`. This doesn't make sense
    because the caller of onError is a different thread than that of await.
    
    This bug caused travis to crash other tests in hard to diagnose ways,
    notably AsyncReporterTest.flush_incrementsMetrics

--- core/src/main/java/zipkin/reporter/internal/AwaitableCallback.java
+++ core/src/main/java/zipkin/reporter/internal/AwaitableCallback.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 The OpenZipkin Authors
+ * Copyright 2016-2017 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -41,7 +41,7 @@ public final class AwaitableCallback implements Callback {
           if (result instanceof Throwable) {
             if (result instanceof Error) throw (Error) result;
             if (result instanceof RuntimeException) throw (RuntimeException) result;
-            if (result instanceof InterruptedException) interrupted = true;
+            // Don't set interrupted status when the callback received InterruptedException
             throw new RuntimeException((Throwable) result);
           }
         } catch (InterruptedException e) {
