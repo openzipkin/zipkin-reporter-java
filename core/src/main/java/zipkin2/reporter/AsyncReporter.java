c2a555d4fd2683b48a37e9684e26f51174247987
Adrian Cole <adriancole@users.noreply.github.com>
2019-06-14 11:17:33 +0800

    Restores OpenZipkin publishing configuration (#148)

--- core/src/main/java/zipkin2/reporter/AsyncReporter.java
+++ core/src/main/java/zipkin2/reporter/AsyncReporter.java
@@ -1,18 +1,15 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright 2016-2019 The OpenZipkin Authors
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package zipkin2.reporter;
 
@@ -52,7 +49,7 @@ import static java.util.logging.Level.FINE;
  */
 public abstract class AsyncReporter<S> extends Component implements Reporter<S>, Flushable {
   /**
-   * Builds a json reporter for <a href="https://zipkin.apache.org/zipkin-api/#/">Zipkin V2</a>. If http,
+   * Builds a json reporter for <a href="https://zipkin.io/zipkin-api/#/">Zipkin V2</a>. If http,
    * the endpoint of the sender is usually "http://zipkinhost:9411/api/v2/spans".
    *
    * <p>After a certain threshold, spans are drained and {@link Sender#sendSpans(List) sent} to
