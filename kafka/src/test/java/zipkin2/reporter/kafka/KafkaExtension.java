a876d2e2c8f1fc1fbe2b433d105b85d933012890
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2021-11-01 06:57:24 +0000

    chore: describe exposing kafka listener post on testcontainers (#203)
    * chore: describe exposing kafka listener post on testcontainers
    
    * chore: update headers

--- kafka/src/test/java/zipkin2/reporter/kafka/KafkaExtension.java
+++ kafka/src/test/java/zipkin2/reporter/kafka/KafkaExtension.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2020 The OpenZipkin Authors
+ * Copyright 2016-2021 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -95,8 +95,7 @@ class KafkaExtension implements BeforeAllCallback, AfterAllCallback {
         throw new TestAbortedException("${docker.skip} == true");
       }
       waitStrategy = Wait.forHealthcheck();
-      // 19092 is for connections from the Docker host and needs to be used as a fixed port.
-      // TODO: someone who knows Kafka well, make ^^ comment better!
+      // Kafka broker listener port (19092) needs to be exposed for test cases to access it.
       addFixedExposedPort(KAFKA_PORT, KAFKA_PORT, InternetProtocol.TCP);
       withLogConsumer(new Slf4jLogConsumer(LOGGER));
     }
