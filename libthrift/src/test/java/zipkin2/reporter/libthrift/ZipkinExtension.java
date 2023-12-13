48bedfebb5b823b6c52832d09c65bfe915da0216
Adrian Cole <adrian@tetrate.io>
2023-12-14 06:23:24 +0700

    deps: updates to latest notably zipkin
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- libthrift/src/test/java/zipkin2/reporter/libthrift/ZipkinExtension.java
+++ libthrift/src/test/java/zipkin2/reporter/libthrift/ZipkinExtension.java
@@ -76,7 +76,7 @@ class ZipkinExtension implements BeforeAllCallback, AfterAllCallback {
   // mostly waiting for https://github.com/testcontainers/testcontainers-java/issues/3537
   static final class ZipkinContainer extends GenericContainer<ZipkinContainer> {
     ZipkinContainer() {
-      super(parse("ghcr.io/openzipkin/zipkin:2.24.4"));
+      super(parse("ghcr.io/openzipkin/zipkin:2.25.0"));
       if ("true".equals(System.getProperty("docker.skip"))) {
         throw new TestAbortedException("${docker.skip} == true");
       }
